d5eba0d62d1375c26d9ce8c9e4e3468424232d4a
Guillaume Ferrier <guillaume.ferrier@telecom-paristech.fr>
2018-10-25 16:29:55 +0200

    regexp debut

--- Lesson4/exo_cc_lesson_04.py
+++ Lesson4/exo_cc_lesson_04.py
@@ -1,12 +1,19 @@
 import requests
 urlpara = "https://www.open-medicaments.fr/api/v1/medicaments?limit=100&query=paracetamol"
-
+urlmed = "https://www.open-medicaments.fr/api/v1/medicaments/{}"
 j = requests.get(urlpara).content
 df = pandas.DataFrame.from_records(j)
-# ne marche pas
-df['precomma'] = df['denomination'].str.split(pat=',',n=1,expand=False)
 
+# ne marche pas
+#df['precomma'] = df['denomination'].str.split(pat=',',n=1,expand=False)
+df['dosage'] = df['denomination'].str.extract(pat=' ([0-9]*) [mk]+g',expand=False)
+df['unit'] = df['denomination'].str.extract(pat=' [0-9]* ([mk]+g)',expand=False)
+
+df['previrg'] = df['denomination'].str.extract('^(.*),')
+df['comprime'] = df['denomination'].str.extract(',(.*)$')
 
 #https://www.open-medicaments.fr/views/display.html
-#autre api details :/api/v1/medicaments/{id}
+#autre api details :
+
+#https://www.open-medicaments.fr/api/v1/medicaments/67445776
 
